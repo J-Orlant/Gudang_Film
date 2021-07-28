6f37c3c54751c81fa311cf1ff75c7706a5f9b39a
HadesCry <88101754+HadesCry@users.noreply.github.com>
2021-07-28 23:12:28 +0700

    Update film1

--- database/seeders/FilmSeeder.php
+++ database/seeders/FilmSeeder.php
@@ -27,10 +27,10 @@ class FilmSeeder extends Seeder
             'release' => 2019,
         ]);
         Film::create([
-            'banner' => 'alita.jpg',
-            'video' => 'alita.mp4',
-            'trailer' => 'alitatrailer.mp4',
-            'name' => 'Alita',
+            'banner' => 'bumblebee.jpg',
+            'video' => 'bumblebee.mp4',
+            'trailer' => 'bumblebeetrailer.mp4',
+            'name' => 'bumblebee',
             'synopsis' => 'Alita terbangun di dunia masa depan yang tak ia kenal, dan tanpa ingatan tentang siapa dirinya. Ia kemudian dibawa oleh Ido, seorang dokter simpatik yang menyadari bahwa di dalam tubuh robot Alita yang sempat terbengkalai itu terdapat hati dan jiwa seorang wanita muda dengan kisah masa lalu yang luar biasa',
             'producer' => 'James Cameron, Jon Landau, David Valdes',
             'duration' => '2 hours 2 min',
